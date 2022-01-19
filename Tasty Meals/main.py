ef51d02b7d5f162ba49caafd2877345327659cdf
Armaan235 <armaanarora02.singh@gmail.com>
2022-01-19 22:50:28 +0530

    v

---
+++ Tasty Meals/main.py
@@ -0,0 +1,5 @@
+print("hello")
+def Func():
+    x="world"
+    print(x)
+Func()
---
+++ Tasty Meals/scriptbutton.js
@@ -0,0 +1,20 @@
+function togglehide(){
+    let btn = document.getElementById('button');
+    let para = document.getElementById('para');
+    if(para.style.display!= 'none'){
+    para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
+function togglehide2(){
+    let btn = document.getElementById('button2');
+    let para = document.getElementById('para');
+    if(para.style.display!= 'none'){
+    para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
